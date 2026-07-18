108a1acc1b904be23cf963c759617983a30e1b99
unfool.it <ermeticadesign@gmail.com>
2026-07-18 16:00:28 +0200

    Simplify TypeScript configuration
    Removed several strict type checking options and updated include/exclude patterns.

--- tsconfig.js
+++ tsconfig.js
@@ -1,37 +1,15 @@
 {
   "compilerOptions": {
-    /* Type Checking */
     "strict": true,
-    "noImplicitAny": true,
-    "strictNullChecks": true,
-    "strictFunctionTypes": true,
-    "strictBindCallApply": true,
-    "strictPropertyInitialization: true,
-    "noImplicitThis": true,
-    "useUnknownInCatchVariables": true,
-    "alwaysStrict": true,
-    "noUnusedLocals": true,
-    "noUnusedParameters": true,
-    "exactOptionalPropertyTypes": true,
-    "noImplicitReturns": true,
-    "noFallthroughCasesInSwitch": true,
-    "noUncheckedIndexedAccess": true,
-    "noImplicitOverride": true,
-    "noPropertyAccessFromIndexSignature": true,
-
-    /* Modules */
     "target": "ESNext",
     "module": "NodeNext",
     "moduleResolution": "NodeNext",
     "rootDir": "./",
     "outDir": "./dist",
-    "removeComments": true,
     "esModuleInterop": true,
     "forceConsistentCasingInFileNames": true,
-
-    /* Completeness */
     "skipLibCheck": true
   },
-  "include": ["src/**/*", "proxy.ts"],
-  "exclude": ["node_modules", "dist"]
+  "include": ["src/**/*"],
+  "exclude": ["node_modules", "dist", "proxy.ts", "proxy.js"]
 }
